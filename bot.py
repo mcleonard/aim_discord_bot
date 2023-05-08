5a7db5b997c1c658cf29dd449e1f55cbecaa877c
Mat Leonard <leonard.mat@gmail.com>
2023-05-08 10:33:24 -0700

    implement Discord bot

--- bot.py
+++ bot.py
@@ -1,70 +1,32 @@
-from pathlib import Path
+import os
 
-from langchain.chains import RetrievalQA
-from langchain.document_loaders import UnstructuredMarkdownLoader
-from langchain.indexes import VectorstoreIndexCreator
-from langchain.llms import OpenAI
-from langchain.prompts import PromptTemplate
+import discord
 
+from qa_retrieval import build_qa
 
-def load_documentation(doc_dir: str):
-    """Load Markdown documentation from a directory"""
-    path = Path(doc_dir)
-    if not path.is_dir:
-        raise ValueError("Input path must be a directory")
+intents = discord.Intents(messages=True)
+intents.message_content = True
 
-    markdown_files = path.glob("**/*.md")
+client = discord.Client(intents=intents)
 
-    loaders = [
-        UnstructuredMarkdownLoader(str(filepath.resolve()))
-        for filepath in markdown_files
-    ]
-    return loaders
+qa = build_qa("../aim/docs/source")
 
 
-question_prompt_template = """You are a help bot for an open source software community.
-Use this section of the documentation to answer a user's question, if the text is related
-to the question:
-{context}
-Question: {question}
-Please include any Python code that is relevant to the answer.
-Relevant text, if any:"""
-QUESTION_PROMPT = PromptTemplate(
-    template=question_prompt_template, input_variables=["context", "question"]
-)
-
-combine_prompt_template = """Given the following summaries from the Aim documentation, 
-collect them into one combined answer for the user's question. If you don't have a good 
-answer, just say that you don't know.
-
-Summaries:
-{summaries}
-
-Question: {question}
-
-Combined answer, if any:
-"""
-
-COMBINE_PROMPT = PromptTemplate(
-    template=combine_prompt_template, input_variables=["summaries", "question"]
-)
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
 
 
-if __name__ == "__main__":
-    loaders = load_documentation("../aim/docs/source")
-    index = VectorstoreIndexCreator().from_loaders(loaders)
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-    llm = OpenAI(temperature=0.2)
+    if message.content.startswith("/question"):
+        await message.channel.send("One second, searching the documentation...")
 
-    qa = RetrievalQA.from_chain_type(
-        llm=llm,
-        chain_type="map_reduce",
-        retriever=index.vectorstore.as_retriever(),
-        chain_type_kwargs={
-            "question_prompt": QUESTION_PROMPT,
-            "combine_prompt": COMBINE_PROMPT,
-        },
-    )
+        answer = qa.run(message.content)
+        await message.channel.send(answer)
 
-    query = "How doing I use Aim with HuggingFace spaces?"
-    print(qa.run(query))
+
+client.run(os.environ["DISCORD_TOKEN"])
